e7b7ef38f508193106e5241b7bd03b0170039c00
Melissa Martinez <58582650+melissmartinez@users.noreply.github.com>
2020-02-05 17:21:19 -0800

    Add files via upload

---
+++ Human/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Human
+{
+    class Human  
+    {
+        //fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        //add a public getter property to access heath
+        public int health
+        {
+            get { return health; }
+        }
+
+        //add constrcutor that takes a value to set Name, and set the remanining fields to default values
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+        //add constructor to assign custom values to all fields
+        public Human(string name, int strength, int intelligence, int dexterity, int hp)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            health = hp;
+        }
+        //attack method
+        public int Attack(Human target)
+        {
+            int dmg = Strength * 5;
+            target.health -= dmg;
+            return target.health;  
+        }
+    }
+}
